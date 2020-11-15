0479bdfce139b35777976295beea98466466eaa4
Dmitr Morozov <50495488+DmitrTRC@users.noreply.github.com>
2020-11-15 20:06:04 +0100

    Update hello_world.py
    Function Average() : Done  # Made my Vasya

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,10 @@
+def average ( array ):
+    return sum ( array ) / len ( array )
+
 def greeting ( name ):
     print ( f'Hello {name} , glad to see you!' )
 
 
-
+print ( average( [1,2,3,4,5,6,7,8,9] )
 greeting(input('Enter your name ->' ))
 
