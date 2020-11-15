5d90d5200caed6c6e4640d36030b17d04b47776c
Dmitry <9984398@gmail.com>
2020-11-15 20:19:17 +0100

    filter refactored to my_filter()

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,15 @@
-def average ( array ):
-    return sum ( array ) / len ( array )
-
-def greeting ( name ):
-    print ( f'Hello {name} , glad to see you!' )
+def my_filter(array):
+    return [x for x in array if not x % 2]
 
 
-print ( average( [1,2,3,4,5,6,7,8,9] )
-greeting(input('Enter your name ->' ))
+def average(array):
+    return sum(array) / len(array)
 
+
+def greeting(name):
+    print(f'Hello {name} , glad to see you!')
+
+
+print(average([1, 2, 3, 4, 5, 6, 7, 8, 9]))
+print(my_filter([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+greeting(input('Enter your name ->'))
